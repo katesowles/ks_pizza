716261026b8632b427e85495a506d3e10cc97d03
Kate Sowles <katesowles@gmail.com>
2016-03-23 14:36:58 -0700

    this shit works. the logic seems sound. let's commit this sh*t while we're ahead

--- scripts/compute.js
+++ scripts/compute.js
@@ -1,35 +1,90 @@
+function random(min,max) {
+  var randomResult = Math.floor(Math.random() * ((max - min) + 1) + min);
+  return randomResult;
+}
+
 var shops = {
   colHeadings : ["Time", "# Pizzas", "# Deliveries", "# Drivers"],
   timeBlocks : ["8:00 - 8:59am", "9:00 - 9:59am", "10:00 - 10:59am", "11:00 - 11:59am", "12:00 - 12:59pm", "1:00 - 1:59pm", "2:00 - 2:59pm", "3:00 - 3:59pm", "4:00 - 4:59pm", "5:00 - 5:59pm", "6:00 - 6:59pm", "7:00 - 7:59pm", "8:00 - 8:59pm", "9:00 - 9:59pm", "10:00 - 10:59pm", "11:00 - 11:59pm", "12:00 - 12:59am", "1:00 - 1:59am"],
   location : ["Beaverton", "Hillsboro", "Downtown", "Northeast", "Clackamas", "Airport"],
   pieRange : [[0,4], [0,7], [2,15], [15,35], [12,31], [5,20]],
   delRange : [[0,4], [0,4], [1,4], [3,8], [5,12], [6,11]],
-  pizzas : function(min,max) {
-    var pizzaResult = Math.floor(Math.random() * (max - min)) + min;
-    return pizzaResult;
+};
+
+console.log("BLOCK ONE Pizza Min/Max (should be between 0-4):    " + random(shops.pieRange[0][0],shops.pieRange[0][1]));
+console.log("BLOCK ONE Delivery Min/Max (should be between 0-4): " + random(shops.delRange[0][0],shops.delRange[0][1]));
+console.log("----------------------------------------------------------");
+console.log("BLOCK TWO Pizza Min/Max (should be between 0-7):    " + random(shops.pieRange[1][0],shops.pieRange[1][1]));
+console.log("BLOCK TWO Delivery Min/Max (should be between 0-4): " + random(shops.delRange[1][0],shops.delRange[1][1]));
+console.log("----------------------------------------------------------");
+console.log("BLOCK THREE Pizza Min/Max (should be between 2-15):   " + random(shops.pieRange[2][0],shops.pieRange[2][1]));
+console.log("BLOCK THREE Delivery Min/Max (should be between 1-4): " + random(shops.delRange[2][0],shops.delRange[2][1]));
+console.log("----------------------------------------------------------");
+console.log("BLOCK FOUR Pizza Min/Max (should be between 15-35):  " + random(shops.pieRange[3][0],shops.pieRange[3][1]));
+console.log("BLOCK FOUR Delivery Min/Max (should be between 3-8): " + random(shops.delRange[3][0],shops.delRange[3][1]));
+console.log("----------------------------------------------------------");
+console.log("BLOCK FIVE Pizza Min/Max (should be between 12-31):   " + random(shops.pieRange[4][0],shops.pieRange[4][1]));
+console.log("BLOCK FIVE Delivery Min/Max (should be between 5-12): " + random(shops.delRange[4][0],shops.delRange[4][1]));
+console.log("----------------------------------------------------------");
+console.log("BLOCK SIX Pizza Min/Max (should be between 5-20):    " + random(shops.pieRange[5][0],shops.pieRange[5][1]));
+console.log("BLOCK SIX Delivery Min/Max (should be between 6-11): " + random(shops.delRange[5][0],shops.delRange[5][1]));
+console.log("----------------------------------------------------------");
+console.log("number of items in the location array: " + shops.location.length);
+console.log("number of items in the colHeadings array: " + shops.colHeadings.length);
+console.log("number of items in the timeBlocks array: " + shops.timeBlocks.length);
+console.log("----------------------------------------------------------");
+var pizzaResult;
+var deliveryResult;
+var driversResult
+var my = {
+  pizzas : function(x) {
+    // console.log("pizzas needed: " + x); //x should denote the array from which we're pulling the min/max for that time block's pizza production
+    pizzaResult = random(shops.pieRange[x][0],shops.pieRange[x][1]);
+    // console.log("#Pizzas: " + random(shops.pieRange[x][0],shops.pieRange[x][1]));
   },
-  deliveries : function(min,max) {
-    var deliveryResult = Math.floor(Math.random() * (max - min)) + min;
-    return deliveryResult;
+  deliveries : function(y) {
+    this.pizzas(y);
+    console.log("# Pizzas: " + pizzaResult);
+    console.log("Potential Deliveries: " + shops.delRange[y][1]);
+    deliveryResult = 0;
+    if (pizzaResult == 0) { // when pizza = 0, make the deliveries needed 0
+      deliveryResult = 0;
+      console.log("Modified Deliveries (when pizza = 0, deliveries = 0): " + deliveryResult);
+      return deliveryResult;
+    }
+    else if (pizzaResult >= shops.delRange[y][0]) { // when there are more pizzas baked than the minimum deliveries, make the # deliveries a new minimum of 1
+      deliveryResult = random(1, pizzaResult);
+      console.log("Modified Deliveries (when max is more than number of pizzas made): " + deliveryResult);
+      return deliveryResult;
+    }
+    else if (pizzaResult < shops.delRange[y][0]) {
+      deliveryResult = random(1, pizzaResult);
+      console.log("Modified Deliveries (when min is less than number of pizzas made): " + deliveryResult);
+      return deliveryResult;
+    }
+    else {
+      console.error("the fuck did you do?");
+    }
+    if (deliveryResult > pizzaResult) {
+      console.error("ruh roh shaggy, there are too many deliveries!");
+    }
   },
-  drivers : function(i) {
-    var driversResult = Math.ceil(pizzas(i)/3);
+  drivers : function(z) {
+    this.pizzas(z);
+    this.deliveries(z);
+    console.log("# Pizzas: " + pizzaResult);
+    console.log("# Deliveries: " + deliveryResult);
+    driversResult = Math.ceil(deliveryResult / 3);
+    console.log("DRIVER RESULT: " + driversResult);
     return driversResult;
   }
 };
-
-console.log(shops.pizzas(shops.pieRange[0][0],shops.pieRange[0][1]));
-console.log(shops.deliveries(shops.delRange[0][0],shops.delRange[0][1]));
 
 
 /**********************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************/
 
 
-console.log("number of items in the location array: " + shops.location.length);
 
-console.log("number of items in the colHeadings array: " + shops.colHeadings.length);
-
-console.log("number of items in the timeBlocks array: " + shops.timeBlocks.length);
 
 for (i = 0; i < shops.location.length; i++) {
 
@@ -115,21 +170,21 @@ for (i = 0; i < shops.location.length; i++) {
 
   // creates the numPizza <td> and adds it the <tr> (this is the second #Pizza column)
   var numPizzas = document.createElement("td");
-  var numPizzasText = document.createTextNode("test # pizzas");
+  var numPizzasText = document.createTextNode("# pizzas");
   numPizzas.appendChild(numPizzasText);
   row.appendChild(numPizzas);
 
   // creates the numDeliveries <td> and adds it the <tr> (this is the third #Deliveries column)
   var numDeliveries = document.createElement("td");
-  var numDeliveriesText = document.createTextNode("test # deliveries");
+  var numDeliveriesText = document.createTextNode("# deliveries");
   numDeliveries.appendChild(numDeliveriesText);
   row.appendChild(numDeliveries);
 
+  // creates the numDrivers <td> and adds it the <tr> (this is the fourth #Drivers column)
   var numDrivers = document.createElement("td");
-  var numDriversText = document.createTextNode("test # drivers");
+  var numDriversText = document.createTextNode("# drivers needed");
   numDrivers.appendChild(numDriversText);
   row.appendChild(numDrivers);
-
 
 } // closes for (k) loop
 
